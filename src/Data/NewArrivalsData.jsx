e0df8c5d8a5490979908ce8bbba0220039b53979
Sudeep Rajbanshi <isudp.th@gmail.com>
2023-10-03 16:40:00 +0545

    Home page New Arrivals section completed

---
+++ src/Data/NewArrivalsData.jsx
@@ -0,0 +1,51 @@
+const NewArrivalsData = [
+    {
+        id:1,
+        productImage:'http://maxima.com.np/media/uploads/cat6A_WyJqCeZ.jpg',
+        productTitle:'Kuwes 24port Patch Panel',
+        productPrice:'0'
+    },
+    {
+        id:2,
+        productImage:'http://maxima.com.np/media/uploads/crimping-tool2.jpg',
+        productTitle:'Kuwes Crimping Tool',
+        productPrice:'2100'
+    },
+    {
+        id:3,
+        productImage:'http://maxima.com.np/media/uploads/punch-down2.jpg',
+        productTitle:'Kuwes Punch Down Tool',
+        productPrice:'2400'
+    },
+    {
+        id:4,
+        productImage:'http://maxima.com.np/media/uploads/E7C432_pXFSCTW.jpg',
+        productTitle:'7-inch 4 MP 32X Powered by DarkFighter IR Network Speed Dome',
+        productPrice:'0'
+    },
+    {
+        id:5,
+        productImage:'http://maxima.com.np/media/uploads/cat6A_WyJqCeZ.jpg',
+        productTitle:'Kuwes 24port Patch Panel',
+        productPrice:'0'
+    },
+    {
+        id:6,
+        productImage:'http://maxima.com.np/media/uploads/24port-patchpanel.jpg',
+        productTitle:'Kuwes 24port Patch Panel',
+        productPrice:'0'
+    },
+    {
+        id:7,
+        productImage:'http://maxima.com.np/media/uploads/crimping-tool.jpg',
+        productTitle:'Kuwes Crimping Tool',
+        productPrice:'2100'
+    },
+    {
+        id:8,
+        productImage:'http://maxima.com.np/media/uploads/32_yAX3JpV.jpg',
+        productTitle:'AX PRO with big battery size',
+        productPrice:'32420'
+    },
+]
+export default NewArrivalsData
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import CategoryBanner from '../Components/HomePageComponent/CategoryBanner'
+import NewArrivals from '../Components/HomePageComponent/NewArrivals'
 
 function Home() {
     return (
         <>
          <CategoryBanner/>
+         <NewArrivals/>
         </>
     )
 }
